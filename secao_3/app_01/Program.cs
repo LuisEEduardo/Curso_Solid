c26d8adf276dcd04094af338d859e0078c6d6dae
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-08-11 09:05:36 -0300

    Testando a aplicação

--- secao_3/app_01/Program.cs
+++ secao_3/app_01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using app.Entity;
+
+ICalculaDescontoFidelidade descFide = new CalculaDescontoFidelidade();
+ICalculaDescontoStatusContaFactory descConta = new CalculaDescontosStatusFactory();
+
+GerenciadorDeDescontos gerDesc = new GerenciadorDeDescontos(descConta, descFide);
+
+Console.WriteLine("Valor da compra 1000 e fidelidade 10 anos (5%)\n");
+
+var resultado1 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteComum, 10);
+
+Console.WriteLine($"Cliente {StatusContaCliente.ClienteComum}, valor do desconto é de: {resultado1}");
+var resultado2 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteEspecial, 10);
+Console.WriteLine($"Cliente {StatusContaCliente.ClienteEspecial}, valor do desconto é de: {resultado2}");
+var resultado3 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteVip, 10);
+Console.WriteLine($"Cliente {StatusContaCliente.ClienteVip}, valor do desconto é de: {resultado3}");
+Console.WriteLine($"\n\nValor da compra 1000 e fidelidade 4 anos (4%)\n");
+
+var resultado4 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteComum, 4);
+Console.WriteLine($"Cliente {StatusContaCliente.ClienteComum}, valor do desconto é de: {resultado4}");
+var resultado5 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteEspecial, 4);
+Console.WriteLine($"Cliente {StatusContaCliente.ClienteEspecial}, valor do desconto é de: {resultado5}");
+var resultado6 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteVip, 4);
+Console.WriteLine($"Cliente {StatusContaCliente.ClienteVip}, valor do desconto é de: {resultado6}");
+
+Console.ReadLine();
