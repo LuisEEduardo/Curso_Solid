da2ec7db28931a6adab4f7af03a5456cb1dbd2db
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-08-15 13:59:38 -0300

    Resolução exercício 2

---
+++ secao_3/CursoFoop_Solid_Exercicio2/CursoFoop_Solid_Exercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CursoFoop_Solid_Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ILogger logger = new ConsoleLogger();
+            Pedido pedido = new Pedido(logger);
+
+            pedido.AdicionarPedido();
+
+            Console.ReadLine();
+        }
+    }
+}
